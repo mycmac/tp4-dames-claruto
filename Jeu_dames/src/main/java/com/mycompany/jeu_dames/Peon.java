b3fd97ffa773641aacbd906a58bf4786244c14e0
Clara Mattos <mattosclara22@gmail.com>
2024-12-05 09:09:20 +0100

    Create tableau and peon

---
+++ Jeu_dames/src/main/java/com/mycompany/jeu_dames/Peon.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jeu_dames;
+
+/**
+ *
+ * @author Clara
+ */
+public class Peon {
+    private int x;
+    private int y;
+    private boolean couleur;
+    
+    public Peon (int x, int y, boolean couleur) {
+        this.x = x;
+        this.y = y;
+        this.couleur = couleur;
+               
+    }
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(boolean couleur) {
+        this.couleur = couleur;
+    }
+    
+    public void deplacer() { //TODO
+    }
+    
+    public void prise() { //TODO
+        
+    }
+}
---
+++ Jeu_dames/src/main/java/com/mycompany/jeu_dames/TableauDeJeu.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jeu_dames;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Clara
+ */
+public class TableauDeJeu {
+    public Peon[][] carte;
+    public ArrayList<Peon> peonNoir;
+    public ArrayList<Peon> peonBlanc;
+    
+    public TableauDeJeu() {
+        
+    }
+    
+    public void creerTableau() {
+        
+    }
+    
+    public void tourDeJeu() {
+        
+    
+    }
+    
+    public void sauvegarderPartie() {
+        
+    }
+    
+    public void chargerPartie() {
+        
+    }
+    
+    public void verifierFinPartie() {
+        
+    }
+    
+}

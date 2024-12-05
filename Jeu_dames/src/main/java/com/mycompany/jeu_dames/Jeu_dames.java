6bdc47f2bf5592564624cd932523d0205ea7ee6e
Clara Mattos <mattosclara22@gmail.com>
2024-12-05 08:49:42 +0100

    first commit

---
+++ Jeu_dames/src/main/java/com/mycompany/jeu_dames/Jeu_dames.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jeu_dames;
+
+/**
+ *
+ * @author Clara
+ */
+public class Jeu_dames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

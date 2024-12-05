4e0e841f0a300a6849fdbf063b4b2b556a0c516b
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2024-12-05 10:33:23 +0100

    Effacer les textes par défaut

--- Jeu_dames/src/main/java/com/mycompany/jeu_dames/Joueur.java
+++ Jeu_dames/src/main/java/com/mycompany/jeu_dames/Joueur.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.jeu_dames;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Clara
- */
 public class Joueur {
     private String nom;
     private boolean couleur;

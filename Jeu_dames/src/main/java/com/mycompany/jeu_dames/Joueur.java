81db5d5c868f3803cfbb718ce25688198fad286e
Augusto Arrojo <augusto.arrojo@eleves.ec-nantes.fr>
2024-12-05 10:37:04 +0100

    Fonction pour initializer le nom du joueur

--- Jeu_dames/src/main/java/com/mycompany/jeu_dames/Joueur.java
+++ Jeu_dames/src/main/java/com/mycompany/jeu_dames/Joueur.java
@@ -2,19 +2,52 @@ package com.mycompany.jeu_dames;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a player in the game. A player has a name, a color, and is
+ * associated with a game board.
+ */
 public class Joueur {
+
     private String nom;
-    private boolean couleur;
-    private TableauDeJeu partie;
-    
+    private final boolean couleur;
+    private final TableauDeJeu partie;
+
+    // 
+    /**
+     * Constructor to load a game
+     * <p>
+     * Constructs a player with a specified name, color, and associated game
+     * board. </p>
+     *
+     * @param nom The name of the player.
+     * @param couleur The color of the player (true for black or false for
+     * white).
+     * @param partie The game board associated with the player.
+     */
     public Joueur(String nom, boolean couleur, TableauDeJeu partie) {
         this.nom = nom;
         this.couleur = couleur;
         this.partie = partie;
     }
-    
+
+    /**
+     * Constructs a player for a new game, where the player’s name is
+     * initialized by the player during the game setup.
+     *
+     * @param couleur The color of the player (true for black or false for
+     * white).
+     * @param partie The game board associated with the player.
+     * @param numeroJoueur The number of the player, used to prompt for the
+     * name.
+     */
+    public Joueur(boolean couleur, TableauDeJeu partie, int numeroJoueur) {
+        this.couleur = couleur;
+        this.partie = partie;
+        //this.initializeNom(numeroJoueur);
+    }
+
     public void tourDeJeu() {
-        
+
     }
 
     public String getNom() {
@@ -28,5 +61,5 @@ public class Joueur {
     public TableauDeJeu getPartie() {
         return partie;
     }
-    
+
 }
